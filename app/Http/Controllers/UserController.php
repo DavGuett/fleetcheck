bf19f7cbf487158f5ae502e0b215765f7743f7fc
David Guetten <david.guetten@triveni.com.br>
2026-04-06 13:37:49 -0300

    feat: add partial create user from admin panel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\CreateNewUser;
+use App\Http\Requests\StoreUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -24,5 +27,12 @@ class UserController extends Controller
     public function create(): Response
     {
         return Inertia::render('users/Create');
+    }
+
+    public function store(StoreUserRequest $request, CreateNewUser $creator): RedirectResponse
+    {
+        $creator->create($request->validated());
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
     }
 }
